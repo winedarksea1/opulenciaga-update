41e0f41fcca8ef33f48155a86d27b7cbc4463faf
Clement Mihailescu <clementmihailescu@gmail.com>
2016-11-01 12:30:03 -0400

    Preliminary routes and HTML / CSS for home page

--- server/start.js
+++ server/start.js
@@ -17,7 +17,7 @@ const app = express()
 if (!pkg.isProduction) {
   // Logging middleware (dev & testing only)
   app.use(require('volleyball'))
-}  
+}
 
 module.exports = app
   // We'll store the whole session in a cookie
@@ -33,24 +33,27 @@ module.exports = app
   // Authentication middleware
   .use(passport.initialize())
   .use(passport.session())
-  
+
   // Serve static files from ../public
   .use(express.static(resolve(__dirname, '..', 'public')))
+
+  //Serve our routes
+  .use('/', require('./routes')
 
   // Serve our api
   .use('/api', require('./api'))
 
   // Send index.html for anything else.
-  .get('/*', (_, res) => res.sendFile(resolve(__dirname, '..', 'public', 'index.html')))
+  .get('/*', (req, res) => res.sendFile(resolve(__dirname, '..', 'public', 'index.html'))))
 
 if (module === require.main) {
   // Start listening only if we're the main module.
-  // 
+  //
   // https://nodejs.org/api/modules.html#modules_accessing_the_main_module
   const server = app.listen(
     process.env.PORT || 1337,
     () => {
-      console.log(`--- Started HTTP Server for ${pkg.name} ---`)      
+      console.log(`--- Started HTTP Server for ${pkg.name} ---`)
       console.log(`Listening on ${JSON.stringify(server.address())}`)
     }
   )
