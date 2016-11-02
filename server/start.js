4d518d21f4402d6999bd20213f3b5ec4235046a7
jimkim <kimjames@nyu.edu>
2016-11-02 15:50:21 -0400

    started users routes

--- server/start.js
+++ server/start.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser')
 const {resolve} = require('path')
 const passport = require('passport')
 const productRouter = require('./products');
+const userRouter = require('./users');
 
 // Bones has a symlink from node_modules/APP to the root of the app.
 // That means that we can require paths relative to the app root by
@@ -38,16 +39,14 @@ module.exports = app
   // Serve static files from ../public
   .use(express.static(resolve(__dirname, '..', 'public')))
 
-  //Serve our routes
-  .use('/', require('./routes')
-
   // Serve our api
   .use('/api', require('./api'))
 
   .use('/products', productRouter)
+  .use('/users', userRouter)
 
   // Send index.html for anything else.
-  .get('/*', (req, res) => res.sendFile(resolve(__dirname, '..', 'public', 'index.html'))))
+  .get('/*', (req, res) => res.sendFile(resolve(__dirname, '..', 'public', 'index.html')))
 
 if (module === require.main) {
   // Start listening only if we're the main module.
