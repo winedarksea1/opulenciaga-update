c33754093ade03ee8e4698097d2f13bed5ccf01f
Clement Mihailescu <clementmihailescu@gmail.com>
2016-11-07 10:32:02 -0500

    Added a bit of design

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -8,46 +8,18 @@ export default class Navbar extends React.Component {
     }
     render() {
       return (
-        <div id='navbarDiv'>
-         <nav className="navbar navbar-inverse">
-           <div className="container-fluid">
-             <div className="navbar-header">
-               <a className="navbar-brand" href="#">Opulence</a>
-             </div>
-             <ul className="nav navbar-nav">
-               <li className="dropdown">
-                 <a className="dropdown-toggle" href="#"><Link to="/shoelace">Shoelaces</Link></a>
-                 <ul className="dropdown-menu">
-                   <li><a href="#">For His Majesty</a></li>
-                   <li><a href="#">For Her Majesty</a></li>
-                 </ul>
-               </li>
-               <li className="dropdown">
-                 <a className="dropdown-toggle" href="#"><Link to="/cufflink">Cufflinks</Link></a>
-                 <ul className="dropdown-menu">
-                   <li><a href="#">For His Majesty</a></li>
-                   <li><a href="#">For Her Majesty</a></li>
-                 </ul>
-               </li>
-               <li className="dropdown">
-                 <a className="dropdown-toggle" href="#"><Link to="/tie">Ties</Link></a>
-                 <ul className="dropdown-menu">
-                   <li><a href="#">For His Majesty</a></li>
-                   <li><a href="#">For Her Majesty</a></li>
-                 </ul>
-               </li>
-               <li className="dropdown">
-                 <a className="dropdown-toggle" href="#"><Link to="/hat">Hats</Link></a>
-                 <ul className="dropdown-menu">
-                   <li><a href="#">For His Majesty</a></li>
-                   <li><a href="#">For Her Majesty</a></li>
-                 </ul>
-               </li>
-             </ul>
-
-           </div>
-         </nav>
+        <div id="navbar">
+          <div id="navbarButtons">
+            <ul>
+              <li><img src="https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwjgptD_35LQAhUq64MKHYYXCtQQjRwIBw&url=http%3A%2F%2Fwww.logospike.com%2Flogo-image-1704%2F&psig=AFQjCNEIiQK_aWiMpqNHAmkSD7J9LX3J_A&ust=1478474270444558"/></li>
+              <li id='shoelaces'><a href="#"><Link to="/shoelace">Shoelaces</Link></a></li>
+              <li id='perfumes'><a href="#"><Link to="/cufflink">Perfumes</Link></a></li>
+              <li id='ties'><a href="#"><Link to="/tie">Ties</Link></a></li>
+              <li id='hats'><a href="#"><Link to="/hat">Hats</Link></a></li>
+            </ul>
+          </div>
         </div>
+
       )
     }
 };
